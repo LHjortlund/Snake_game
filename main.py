9cdbc6596a02fbf1f4b34d36b7ca839406ffc11d
Lyanne Fernandez <lbfe44184@edu.ucl.dk>
2024-09-05 13:50:51 +0200

    adding class

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from turtle import Turtle, Screen
+from snake import Snake
 import time
 
 screen = Screen()
@@ -7,18 +8,7 @@ screen.bgcolor("black")
 screen.title("My Snake Game")
 screen.tracer(0)
 
-# Create snake body and Move the snake
-starting_positions = [(0,0), (-20, 0), ( -40,0)]
-
-segments = []
-
-"""Creating the snake"""
-for position in starting_positions:
-    new_segment = Turtle("square")
-    new_segment.color("white")
-    new_segment.penup()
-    new_segment.goto(position)
-    segments.append(new_segment)
+snake = Snake()
 
 """Moving snake forward"""
 game_is_on = True
@@ -26,11 +16,9 @@ while game_is_on:
     screen.update()
     time.sleep(0.1)
 
-    for seg_num in range(len(segments) -1, 0, -1):
-        new_x = segments[seg_num - 1].xcor()
-        new_y = segments[seg_num - 1].ycor()
-        segments[seg_num].goto(new_x, new_y)
-    segments[0].forward(20)
+    snake.move()
+
+
 
 
 # TODO  Control the snake
