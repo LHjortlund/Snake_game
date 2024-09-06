079eb6faa83c4dbaf0ffb65cd40b274b57744870
Lyanne Fernandez <lbfe44184@edu.ucl.dk>
2024-09-06 09:51:59 +0200

    collision with tail added

--- main.py
+++ main.py
@@ -30,6 +30,7 @@ while game_is_on:
     #Detect collision with food and add points
     if snake.head.distance(food) < 15:
         food.refresh()
+        snake.extend()
         scoreboard.increase_score()
 
     #Detect collision with wall
@@ -37,9 +38,12 @@ while game_is_on:
         game_is_on = False
         scoreboard.game_over()
 
-
-
-
-# Detect collision with tail
+    #Detect collision with tail
+    for segment in snake.segments:
+        if segment == snake.head:
+            pass
+        elif snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
 
 screen.exitonclick()
